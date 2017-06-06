b735b9e8ebd2a24b02bfa51b0c20b6dffb49d531
virgesmith <andrew@casacazaz.net>
2017-06-06 09:46:04 +0100

    added 1d historgrams of pseudo vs quasi reference Lenormand (TODO add ref to bib) mentioned ensemble

--- QIWS/PvQ.R
+++ QIWS/PvQ.R
@@ -3,6 +3,10 @@ N=10000
 M=10
 B=M*M
 
+# raw histograms
+hist(runif(N),breaks=99, main="", xlab="Uniform pseudorandom variates")
+hist(humanleague::sobolSequence(1,N), breaks=99, main="", xlab="Uniform quasirandom variates")
+
 p=hist(runif(N),breaks=B-1,plot=F)$counts
 p2=matrix(p,nrow=M)
 # 1d Sobol sequence
@@ -10,13 +14,13 @@ p2=matrix(p,nrow=M)
 
 #2d Sobol sequence
 s=humanleague::sobolSequence(2,N)
-s=floor(s*M+1)
+s=floor(s*M+1) # turn [0,1) into buckets
 q2=matrix(rep(0,B),nrow=M)
 
 # sample in square
 for (i in 1:N) {
-  x = s2[i,1]
-  y = s2[i,2]
+  x = s[i,1]
+  y = s[i,2]
   #print(paste(x,y))
   q2[x,y] = q2[x,y]+1
 }
@@ -26,3 +30,4 @@ s2dp=matrix(p,nrow=M)
 pc=chisq.test(p2)
 qc=chisq.test(q2)
 print(pc)
+print(qc)
